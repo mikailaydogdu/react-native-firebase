93b6e03a3b1a481ec2c4986af3fb20a4242fd71c
Mikail Aydoğdu <40839224+mikailaydogdu@users.noreply.github.com>
2023-06-23 15:37:53 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import {getAuth} from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuN7O0scQoRadfqeLcBqEklrYelTG2mAk",
-  authDomain: "fir-auth-537be.firebaseapp.com",
-  projectId: "fir-auth-537be",
-  storageBucket: "fir-auth-537be.appspot.com",
-  messagingSenderId: "260850582260",
-  appId: "1:260850582260:web:836b0874118be8a238d4a1"
+ //
 };
 
 
